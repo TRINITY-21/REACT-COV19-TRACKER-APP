2a80d15029d21bf178b487762b9f4b13cd4aff50
= <=>
2020-07-04 17:59:29 -0700

    Updates

---
+++ precache-manifest.83303feb6f1caff3dfe8af2c00cef072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362b9824fed219341f53798558e3b705",
+    "url": "/REACT-COV19-TRACKER-APP/index.html"
+  },
+  {
+    "revision": "dfd1dcb8f5113cbb74c5",
+    "url": "/REACT-COV19-TRACKER-APP/static/css/main.8216d078.chunk.css"
+  },
+  {
+    "revision": "a123265dce142062a597",
+    "url": "/REACT-COV19-TRACKER-APP/static/js/2.0b04d18f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/REACT-COV19-TRACKER-APP/static/js/2.0b04d18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd1dcb8f5113cbb74c5",
+    "url": "/REACT-COV19-TRACKER-APP/static/js/main.3eeb91fd.chunk.js"
+  },
+  {
+    "revision": "5c052b768723f7ad735f",
+    "url": "/REACT-COV19-TRACKER-APP/static/js/runtime-main.32409f4d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/REACT-COV19-TRACKER-APP/static/media/image.d7265326.png"
+  }
+]);
