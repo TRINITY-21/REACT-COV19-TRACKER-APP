241412015d2b1a3e37054245f4e8608d90646c84
= <=>
2020-07-10 23:03:58 -0700

    Updates

---
+++ precache-manifest.f502c1126c4eccdc80e984ec0cab7d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dedb3ccf30148ffbad3e73ec951abe6",
+    "url": "/REACT-MOVIEAPP/index.html"
+  },
+  {
+    "revision": "48df48d41c516e8ef002",
+    "url": "/REACT-MOVIEAPP/static/css/main.a8335d3b.chunk.css"
+  },
+  {
+    "revision": "232fbae20f03348b2afa",
+    "url": "/REACT-MOVIEAPP/static/js/2.3eaa6af8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACT-MOVIEAPP/static/js/2.3eaa6af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48df48d41c516e8ef002",
+    "url": "/REACT-MOVIEAPP/static/js/main.d48d33a8.chunk.js"
+  },
+  {
+    "revision": "0a57184892fdac2e9a8f",
+    "url": "/REACT-MOVIEAPP/static/js/runtime-main.96f9f65c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/REACT-MOVIEAPP/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REACT-COV19-TRACKER-APP/precache-manifest.46e03b9a03e46d4fbeb17d7d0a8657d4.js"
+  "/REACT-MOVIEAPP/precache-manifest.f502c1126c4eccdc80e984ec0cab7d46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACT-COV19-TRACKER-APP/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACT-MOVIEAPP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
